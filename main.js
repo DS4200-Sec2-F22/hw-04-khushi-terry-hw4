20f36d966daca6e07b95a4fd5275eb0f56732919
tchen2 <chen.ter@husky.neu.edu>
2022-10-06 19:48:39 -0400

    added comments

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+
+// Tracks the total number of clicks to determine whether a circle has been clicked at all.
 let totalClicks =0;
-let circles = document.getElementsByTagName('circle')
+
+// list of all options
+const vals = document.getElementsByTagName('option');
+
+// list of all elements that are circles.
+let circles = document.getElementsByTagName('circle');
+
+// add event listeners to all circles
 let numCircles = circles.length;
 for (var i = 0; i<circles.length;i++) {
-    circles[i].addEventListener('click', changeCircle)
+    circles[i].addEventListener('click', changeCircle);
 }
 
+// function to change the state of the circle (outline/no outline).
 function changeCircle(e) {
     let circleItem = document.getElementById(e.target.id);
     if(totalClicks==0) {
@@ -13,17 +23,17 @@ function changeCircle(e) {
         totalClicks+=1;
         }else if(circleItem.classList.contains('circle-outline') && totalClicks>0) {
             circleItem.classList.remove('circle-outline');
-            coordinates.innerHTML = "Last point clicked:" + e.target.id
+            coordinates.innerHTML = "Last point clicked:" + e.target.id;
             totalClicks+=1;
         } else{
             circleItem.classList.add('circle-outline');
-            coordinates.innerHTML = "Last point clicked:" + e.target.id
+            coordinates.innerHTML = "Last point clicked:" + e.target.id;
             totalClicks+=1;
         }
     }
 
-const vals = document.getElementsByTagName('option');
 
+// Sets the received input from dropdown to create a circle when submitted.
 function submitClicked() {
 
     let x;
@@ -34,23 +44,20 @@ function submitClicked() {
             if (vals[i].id == 'x') {
                 x = vals[i].value;
                 vals[i].selected = true;
-                console.log(x);
             } else if (vals[i].id =='y') {
                 y = vals[i].value;
                 vals[i].selected = true;
-                console.log(y);
             }
         }
     }
 
     addCircle(x, y);
 }
-
 document.getElementById('submit').addEventListener("click", submitClicked);
 
+// creates a new circle with the received attributes that has the same characteristics of other points.
 function addCircle(x, y) {
     let parentElement = document.getElementById('frame');
-    
     let newPoint = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
     let id = "("+x+","+y+")";
     newPoint.setAttribute("id", id);
@@ -63,7 +70,6 @@ function addCircle(x, y) {
 
     circles[numCircles].addEventListener('click', changeCircle);
     numCircles+=1;
-    console.log(frame);
 }
 
 
